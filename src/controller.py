dd4c42c579b5d3c589efe9fb56d476050ab653fa
Jairo Cogollo <mickt681@gmail.com>
2023-09-13 22:34:48 -0500

    codigo funcional en el servidor

--- src/controller.py
+++ src/controller.py
@@ -8,29 +8,34 @@ class Convertir:
         if not file.filename.endswith(".rar"):
             return {"error": "El archivo debe tener extensión .rar"}
 
-        # Crear una carpeta temporal para almacenar los archivos descomprimidos
-        temp_folder = "temp_folder"
+        # Carpeta donde se almacenan los archivos temporales
+        temp_folder = "uploads"
         os.makedirs(temp_folder, exist_ok=True)
+        # Descomprimir el archivo RAR en la carpeta "downloads"
+        download_folder = "downloads"
+        os.makedirs(download_folder, exist_ok=True)
 
         # Guardar el archivo RAR en la carpeta temporal
         with open(os.path.join(temp_folder, file.filename), "wb") as f:
             shutil.copyfileobj(file.file, f)
 
-        # Descomprimir el archivo RAR
+        
         with rarfile.RarFile(os.path.join(temp_folder, file.filename), 'r') as rf:
-            rf.extractall(temp_folder)
+            rf.extractall(download_folder)
 
         # Crear un archivo ZIP a partir de los archivos descomprimidos
-        archivo_zip = os.path.splitext(file.filename)[0] + ".zip"
+        archivo_zip = os.path.join(download_folder, os.path.splitext(file.filename)[0] + ".zip")
         with zipfile.ZipFile(archivo_zip, 'w', zipfile.ZIP_DEFLATED) as zf:
-            for root, _, files in os.walk(temp_folder):
+            for root, _, files in os.walk(download_folder):
                 for archivo in files:
                     ruta_completa = os.path.join(root, archivo)
-                    arcname = os.path.relpath(ruta_completa, temp_folder)
+                    arcname = os.path.relpath(ruta_completa, download_folder)
                     zf.write(ruta_completa, arcname)
 
-        # Eliminar la carpeta temporal
-        shutil.rmtree(temp_folder)
-
+        # Eliminar solo los archivos temporales, pero mantener la carpeta "uploads"
+        # for archivo_temporal in os.listdir(temp_folder):
+        #     archivo_temporal_path = os.path.join(temp_folder, archivo_temporal)
+        #     if os.path.isfile(archivo_temporal_path):
+        #         os.remove(archivo_temporal_path)
 
         return archivo_zip
